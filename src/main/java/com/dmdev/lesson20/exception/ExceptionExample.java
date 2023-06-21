6dea7f3e831a5cd539ecc984826d5449e3a60a1f
Viner108 <andrukovichl@mail.ru>
2023-06-21 16:06:30 +0300

    video 2(исключения) commit

--- src/main/java/com/dmdev/lesson20/exception/ExceptionExample.java
+++ src/main/java/com/dmdev/lesson20/exception/ExceptionExample.java
@@ -4,24 +4,33 @@ import java.io.FileNotFoundException;
 import java.util.concurrent.TimeoutException;
 
 public class ExceptionExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, TimeoutException {
         System.out.println("main start");
         try {
+            System.out.println(finallyTest());
             unsafe(10);
-        } catch (TimeoutException exception){
-            // handle exception
+        }catch (RuntimeException exception){
+            System.out.println("catch");
             exception.printStackTrace();
-        } catch (Exception exception){
-            // handle exception
-            exception.printStackTrace();
+        } finally {
+            System.out.println("finally");
         }
         System.out.println("main end");
         // code
     }
-    public static void unsafe(int value) throws FileNotFoundException, TimeoutException {
+    public static int finallyTest(){
+        try {
+            return 2;
+        }catch (Throwable throwable){
+            return 3;
+        }finally {
+            return 4;
+        }
+    }
+    public static void unsafe(int value) {
         System.out.println("unsafe start");
         if (value>0){
-            throw new FileNotFoundException();
+            throw new RuntimeException();
         }
         System.out.println("unsafe end");
         // code
